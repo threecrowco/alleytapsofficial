ea16640c1edc612241c0f68b8a0f0bbba34a2236
Chris Canterbury <threecrowco@Chriss-iMac.local>
2020-03-09 16:45:18 -0500

    removed readme

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -14,6 +14,7 @@
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none">
     <div id="app">
+        @include('layouts.partials.frontend.header')
         @yield('content')
     </div>
 
--- resources/views/layouts/partials/frontend/header.blade.php
+++ resources/views/layouts/partials/frontend/header.blade.php
@@ -1,3 +1,13 @@
-<section class="hidden lg:flex">
-    <div>logo</div>
+<section class="hidden lg:flex justify-center items-center bg-gray-200">
+    <div class="w-5/12 bg-red-300 text-right py-4">
+        <a class="mr-24" href="/">Home</a>
+        <a class="mr-24" href="/">About</a>
+        <a class="mr-0" href="/">Events</a>
+    </div>
+    <div class="w-2/12 bg-blue-300 text-center py-4">logo</div>
+    <div class="w-5/12 bg-red-300 py-4">
+        <a class="ml-0" href="/">Gallery</a>
+        <a class="ml-24" href="/">Booking</a>
+        <a class="ml-24" href="/">Contact</a>
+    </div>
 </section>
